24fb95ff8f373454ddd426bb611b3b60771e9baa
meck <meckgalen@gmail.com>
2023-10-09 01:39:09 +0300

    Project is complete, slide show loop feature added

--- index.js
+++ index.js
@@ -1,55 +1,105 @@
 // Creata a function to show selected image
 (function changeImage() {
     // Create image nodes and array
-    const imgList = document.querySelectorAll('.img');
-    const imgArr = Array.from(imgList);
+    const imgList = document.querySelectorAll('.img')
+    const imgArr = Array.from(imgList)
 
     // Create dot nodes and array
-    const dotList = document.querySelectorAll('.dot');
-    const dotArr = Array.from(dotList);
+    const dotList = document.querySelectorAll('.dot')
+    const dotArr = Array.from(dotList)
 
-    let currentImageIndex = 0;
+    let currentImageIndex = 0
+    let slideshowInterval;
 
-
+    // Set and change classes for elements
     function combineToggle(selectedElement, selectedDot) {
-        selectedElement.classList.toggle('enabled');
-        selectedElement.classList.toggle('disabled');
-        selectedDot.classList.toggle('selected');
-        selectedDot.classList.toggle('unselected');
+        selectedElement.classList.toggle('enabled')
+        selectedElement.classList.toggle('disabled')
+        selectedDot.classList.toggle('selected')
+        selectedDot.classList.toggle('unselected')
     }
 
+    // Create slide show loop
+    function startSlideshow() {
+        slideshowInterval = setInterval(() => {
+            if (currentImageIndex === imgArr.length - 1) {
+                reset()
+                currentImageIndex = 0
+                combineToggle(imgArr[currentImageIndex], dotArr[currentImageIndex])
+            } else {
+                currentImageIndex++
+                combineToggle(imgArr[currentImageIndex], dotArr[currentImageIndex])
+                combineToggle(imgArr[currentImageIndex - 1], dotArr[currentImageIndex - 1])
+            }
+        }, 5000)
+    }
+
+    // Stop slide show
+    function stopSlideshow() {
+        clearInterval(slideshowInterval)
+    }
+
+    // Display first image when page loads
     combineToggle(imgArr[currentImageIndex],  dotArr[currentImageIndex])
 
+    // Start slide show
+    startSlideshow()
+
+    // Init click button for next image
     const nextButton = document.querySelector('.arrow-right');
     nextButton.addEventListener('click', () => {
-        console.log('clicked')
-        if(currentImageIndex === 8){
-            return
+        stopSlideshow();
+        if(currentImageIndex === imgArr.length - 1){
+            reset()
+            currentImageIndex = 0
+            combineToggle(imgArr[currentImageIndex], dotArr[currentImageIndex])
         }
         else {
            currentImageIndex++
+           combineToggle(imgArr[currentImageIndex], dotArr[currentImageIndex])
+           combineToggle(imgArr[currentImageIndex - 1], dotArr[currentImageIndex - 1])
         }
-        combineToggle(imgArr[currentImageIndex], dotArr[currentImageIndex])
-        combineToggle(imgArr[currentImageIndex - 1],  dotArr[currentImageIndex - 1])
-        console.log(currentImageIndex)
-        console.log(currentDot);
+        startSlideshow()
     })
 
+    // Init click button for previous image
     const previousButton = document.querySelector('.arrow-left');
     previousButton.addEventListener('click', () => {
-        console.log('clicked')
+        stopSlideshow()
         if(currentImageIndex === 0){
-            return
+            reset()
+            currentImageIndex = imgArr.length - 1
+            combineToggle(imgArr[currentImageIndex], dotArr[currentImageIndex])
         }
         else {
            currentImageIndex--
+           combineToggle(imgArr[currentImageIndex], dotArr[currentImageIndex])
+           combineToggle(imgArr[currentImageIndex + 1], dotArr[currentImageIndex + 1])
         }
-        combineToggle(imgArr[currentImageIndex],  dotArr[currentImageIndex])
-        combineToggle(imgArr[currentImageIndex + 1],  dotArr[currentImageIndex + 1])
-        console.log(currentImageIndex)
-        console.log(currentDot);
+        startSlideshow()
     })
 
-})();
+    // Display selected image
+    dotArr.forEach((item, index) => {
+        item.addEventListener('click', () => {
+            stopSlideshow()
+            currentImageIndex = index
+            reset()
+            combineToggle(imgArr[currentImageIndex], dotArr[currentImageIndex])
+            startSlideshow()
+        })
+    })
 
-// combineToggle(imgArr[8]);
+    // Set all elements to disabled and unselected
+    function reset() {
+        imgArr.forEach((element) => {
+            element.classList.remove('enabled')
+            element.classList.add('disabled')
+        })
+        dotArr.forEach((element) => {
+            element.classList.remove('selected')
+            element.classList.add('unselected')
+        })
+    }
+
+})()
