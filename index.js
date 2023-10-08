12e5da2b86d633ab23d38768e6bf09d9ad0eb2d8
meck <meckgalen@gmail.com>
2023-10-08 15:48:56 +0300

    Slider works, selected dot feature will be added

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
-const imgList = document.querySelectorAll('.img');
-const imgArr = Array.from(imgList);
+// Creata a function to show selected image
+(function changeImage() {
+    // Create image nodes and array
+    const imgList = document.querySelectorAll('.img');
+    const imgArr = Array.from(imgList);
 
-const dotList = document.querySelectorAll('.dot');
-const dotArr = Array.from(dotList);
+    // Create dot nodes and array
+    const dotList = document.querySelectorAll('.dot');
+    const dotArr = Array.from(dotList);
 
-function combineToggle(selectedElement) {
-    selectedElement.classList.toggle('enabled');
-    selectedElement.classList.toggle('disabled');
-}
+    let currentImageIndex = 0;
 
-const nextButton = document.querySelector('.arrow-right');
-nextButton.addEventListener('click', () => {
-    console.log('clicked')
-})
 
-const previousButton = document.querySelector('.arrow-left');
-previousButton.addEventListener('click', () => {
-    console.log('clicked')
-})
+    function combineToggle(selectedElement, selectedDot) {
+        selectedElement.classList.toggle('enabled');
+        selectedElement.classList.toggle('disabled');
+        selectedDot.classList.toggle('selected');
+        selectedDot.classList.toggle('unselected');
+    }
 
+    combineToggle(imgArr[currentImageIndex],  dotArr[currentImageIndex])
+
+    const nextButton = document.querySelector('.arrow-right');
+    nextButton.addEventListener('click', () => {
+        console.log('clicked')
+        if(currentImageIndex === 8){
+            return
+        }
+        else {
+           currentImageIndex++
+        }
+        combineToggle(imgArr[currentImageIndex], dotArr[currentImageIndex])
+        combineToggle(imgArr[currentImageIndex - 1],  dotArr[currentImageIndex - 1])
+        console.log(currentImageIndex)
+        console.log(currentDot);
+    })
+
+    const previousButton = document.querySelector('.arrow-left');
+    previousButton.addEventListener('click', () => {
+        console.log('clicked')
+        if(currentImageIndex === 0){
+            return
+        }
+        else {
+           currentImageIndex--
+        }
+        combineToggle(imgArr[currentImageIndex],  dotArr[currentImageIndex])
+        combineToggle(imgArr[currentImageIndex + 1],  dotArr[currentImageIndex + 1])
+        console.log(currentImageIndex)
+        console.log(currentDot);
+    })
+
+})();
+
+// combineToggle(imgArr[8]);
