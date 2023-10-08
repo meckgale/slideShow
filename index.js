29beff63f2ec03898b6fb44a1e81c12b8493713d
meck <meckgalen@gmail.com>
2023-10-08 05:01:27 +0300

    javascript in progress

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const imgList = document.querySelectorAll('.img');
 const imgArr = Array.from(imgList);
 
+const dotList = document.querySelectorAll('.dot');
+const dotArr = Array.from(dotList);
 
-function next() {
-
+function combineToggle(selectedElement) {
+    selectedElement.classList.toggle('enabled');
+    selectedElement.classList.toggle('disabled');
 }
+
+const nextButton = document.querySelector('.arrow-right');
+nextButton.addEventListener('click', () => {
+    console.log('clicked')
+})
+
+const previousButton = document.querySelector('.arrow-left');
+previousButton.addEventListener('click', () => {
+    console.log('clicked')
+})
+
